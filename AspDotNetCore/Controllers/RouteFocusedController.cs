9db1778022edc437bffd462253cbd930d6f15e0f
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-03-28 01:00:00 +0300

    asp.net core commmit: - renamed contoller "DotNetCore" to "RouteFocused". - Added another Action endpoint that its route is defined by the HttpGet Attribute - browser is not launched when debugging (updated the setting.json file)

--- AspDotNetCore/Controllers/RouteFocusedController.cs
+++ AspDotNetCore/Controllers/RouteFocusedController.cs
@@ -9,16 +9,15 @@ using System.Threading.Tasks;
 namespace AspDotNetCore.Controllers
 {
     [ApiController]
-    public class DotNetCore : ControllerBase
+    public class RouteFocusedController : ControllerBase
     {
-        [HttpGet]
         [Route("[controller]/[action]")]
         public IActionResult IamAnAction()
         {
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("The route set on this Action was defined by the keywords:");
             sb.AppendLine("[controller] and [action]");
-            sb.AppendLine("[controller] = \"DotNetCore\"");
+            sb.AppendLine("[controller] = \"RouteFocusedController\"");
             sb.AppendLine("[action] = \"IamAnAction\"");
             sb.AppendLine("Changing the name of either the controller or action will change the action URL");
             return Ok(sb.ToString());
@@ -31,6 +30,22 @@ namespace AspDotNetCore.Controllers
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("You called this URL by typing the [domain name]/ConstUrlRoute.");
             sb.AppendLine("The route was defined in Action level");
+            return Ok(sb.ToString());
+        }
+
+        [HttpGet("TryToGetMe", Name = "TryToGetMe Route Name")]
+        public IActionResult RouteByGetAttr()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("This Route was created by using the [HttpGet] Attribute");
+            sb.AppendLine("One Parameter was defined in. Here's the C-tor description for it");
+            sb.AppendLine("// Summary:");
+            sb.AppendLine("//   Creates a new Microsoft.AspNetCore.Mvc.HttpGetAttribute with the given route");
+            sb.AppendLine("//   template.");
+            sb.AppendLine("//");
+            sb.AppendLine("// Parameters:");
+            sb.AppendLine("//   template: The route template. May not be null.");
+            sb.AppendLine("public HttpGetAttribute(string template);");
             return Ok(sb.ToString());
         }
     }
