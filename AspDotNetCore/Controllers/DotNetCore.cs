eab77fdcc4c7f28a84fa3fcafc160f9b29b7fb44
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-03-27 18:51:18 +0300

    DotNetCore commit: replaced old controller with new One. In Addition, updated config setting accordingly

---
+++ AspDotNetCore/Controllers/DotNetCore.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspDotNetCore.Controllers
+{
+    [ApiController]
+    public class DotNetCore : ControllerBase
+    {
+        [HttpGet]
+        [Route("[controller]/[action]")]
+        public IActionResult IamAnAction()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("The route set on this Action was defined by the keywords:");
+            sb.AppendLine("[controller] and [action]");
+            sb.AppendLine("[controller] = \"DotNetCore\"");
+            sb.AppendLine("[action] = \"IamAnAction\"");
+            sb.AppendLine("Changing the name of either the controller or action will change the action URL");
+            return Ok(sb.ToString());
+        }
+
+        [HttpGet]
+        [Route("ConstUrlRoute")]
+        public IActionResult ConstUrl()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("You called this URL by typing the [domain name]/ConstUrlRoute.");
+            sb.AppendLine("The route was defined in Action level");
+            return Ok(sb.ToString());
+        }
+    }
+}
