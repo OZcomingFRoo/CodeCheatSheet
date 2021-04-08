2408d416826f227fe2ae0d0fc83804115d861067
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-04-08 12:14:09 +0300

    Update StringExtension.cs
    Added 3 new Utility functions: ContainsAll, ContainsAny and IsEmailStringValid

--- CommonCSharpUtils/MethodExtension/StringExtension.cs
+++ CommonCSharpUtils/MethodExtension/StringExtension.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CommonCSharpUtils.MethodExtension
+﻿namespace CommonCSharpUtils.MethodExtension
 {
     public static class StringExtension
     {
@@ -15,6 +11,57 @@ namespace CommonCSharpUtils.MethodExtension
         public static bool IsSet(this string str)
         {
             return string.IsNullOrEmpty(str) == false;
+        }
+
+        /// <summary>
+        /// Custom string method to check if ALL of the sub-strings provided are inside the string value
+        /// </summary>
+        /// <param name="str">The string value to check</param>
+        /// <param name="subStrings">The list of sub-strings to check if they're inside the string value</param>
+        /// <returns>True if all of them are contained in the string, else false</returns>
+        public static bool ContainsAll(this string str, params string[] subStrings)
+        {
+            foreach (var subStr in subStrings)
+            {
+                if (str.Contains(subStr) == false) return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Custom string method to check if one of the sub-strings provided is inside the string value
+        /// </summary>
+        /// <param name="str">The string value to check</param>
+        /// <param name="subStrings">The list of sub-strings to check if they're inside the string value</param>
+        /// <returns>True if one of them is contained in the string, else false</returns>
+        public static bool ContainsAny(this string str, params string[] subStrings)
+        {
+            foreach (var subStr in subStrings)
+            {
+                if (str.Contains(subStr)) return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Validates that the string value is a valid email address <br/>
+        /// Note! It does not validate if the email address is a valid destination to send a message.<br/>
+        /// For that, the only real way is to send a message to confirm.
+        /// </summary>
+        /// <param name="emailStr">The string to check</param>
+        /// <returns>True if the string value is a valid email address string, else false</returns>
+        public static bool IsValidEmailString(this string emailStr)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(emailStr)) return false;
+                var addr = new System.Net.Mail.MailAddress(emailStr);
+                return addr.Address == emailStr;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
