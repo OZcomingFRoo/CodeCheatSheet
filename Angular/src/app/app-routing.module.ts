95b000447de8054642678c82d8dfa388533d6187
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-04-12 00:33:54 +0300

    added new component and updated routing

--- Angular/src/app/app-routing.module.ts
+++ Angular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InOutDirectiveComponent } from './components/how-to/in-out-directive/in-out-directive.component';
+import { DefaultPageComponent } from './components/utility/default-page/default-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: InOutDirectiveComponent, path: "HowTo/InputOutput" },
+  { component: DefaultPageComponent, path: "Home" },
+  { component: DefaultPageComponent, path: "DefaultPage" },
+  { component: DefaultPageComponent, path: "Default" },
+  { component: DefaultPageComponent, path: "**" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { InOutDirectiveComponent } from './components/how-to/in-out-directive/in-out-directive.component';
 import { PopupWindowComponent } from './components/utility/popup-window/popup-window.component';
 import { NavComponent } from './components/nav/nav.component';
+import { DefaultPageComponent } from './components/utility/default-page/default-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InOutDirectiveComponent,
     PopupWindowComponent,
-    NavComponent
+    NavComponent,
+    DefaultPageComponent
   ],
   imports: [
     BrowserModule,
