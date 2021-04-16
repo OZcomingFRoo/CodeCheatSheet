80e41d54f1cdec80a57253d011514bbccd77844a
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-04-16 19:36:35 +0300

    Jasmine JS commit : Updated README file plus the shallow testing spec file

--- Angular/src/KarmaJasmine/README.md
+++ Angular/src/KarmaJasmine/README.md
@@ -12,3 +12,14 @@ To change the testing browser, simply follow these steps:
 2. Click on 1 of the links. E.g. I searched for Edge (new Chromium version) and got to this [page](https://www.npmjs.com/package/@chiragrupani/karma-chromium-edge-launcher)
 3. Install it as a dev-dependency , of course, because we don't need it for production.
 4. Be sure to follow the NPM instructions. You usually need to go to the Karma's config file and add the new NPM added + change the brwoser it should lauch. Check the projects Karma JS config file to see what I changed and added
+
+## Notes
+
+### Adding Custom matchers (16-04-2012)
+
+Adding custom matchers seems easy at first, if you're using JavaScript and with this link [page](https://www.tutorialspoint.com/jasminejs/jasminejs_matchers.htm), all seems easy pizzy.
+However, in Typescript, this is not as straight foward as it should.
+I tried this link guide, but really I gave up after 30 minutes, as even the guide creator said, most of the built-in validators cover what you need
+Here's the page if you wanna try to implament a custom matcher in Jasmine JS:
+https://medium.com/bb-tutorials-and-thoughts/angular-how-to-add-jasmine-custom-matchers-in-unit-testing-3edd40f567ec
+
--- Angular/src/KarmaJasmine/shallow-testing.spec.ts
+++ Angular/src/KarmaJasmine/shallow-testing.spec.ts
@@ -1,5 +1,52 @@
+// - Each Spec file will have one decsribe function
+// - Consider this as the container that contains all the tests for a tool (e.g. class like a component)
 describe('Shallow Testing', () => {
+  // - The "it" Sections Can be created multiple time inside a "describe" fucntion
+  // - Cosider this as the a unit test to test one "test case"
   it("Should successed because it's not a real testing case", () => {
+    // - The expect it the main assertion function
+    // - The "expect" can be used multiple times inside the "it"
     expect(true).toBeTruthy();
-  })
+  });
+
+  it('Should Expect multiple "expect" functions', () => {
+    let num = 1337;
+    let title = "Kantan Desu ne";
+    let bool = false;
+    expect(num).toEqual(1337);
+    expect(title).not.toBeNull(); // The "not" inverts the matcher statement
+    expect(bool).toBeDefined();
+  });
+
+  it("Should contain Falsy values", () => {
+    let notSetVar;
+    expect(undefined).toBeFalsy();
+    expect(null).toBeFalsy();
+    expect(0).toBeFalsy();
+    expect('').toBeFalsy();
+    expect("").toBeFalsy();
+    expect(notSetVar).toBeFalsy();
+    expect(false).toBeFalsy();
+    notSetVar = "";
+    expect(notSetVar).toBeFalsy();
+
+    notSetVar += "Now I'm set";
+    expect(-1).not.toBeFalsy();
+    expect(notSetVar).not.toBeFalsy();
+  });
+
+  it("Should contain Truthy values", () => {
+    let func = (asd: number, qwe: number, zxc: number) => { return asd + qwe + zxc; };
+    let action = () => { };
+    expect(1).toBeTruthy();
+    expect(" ").toBeTruthy();
+    expect(true).toBeTruthy();
+    expect(action).toBeTruthy();
+    expect(func).toBeTruthy();
+    expect(func(1, -100, 21)).toBeTruthy();
+
+    expect(action()).not.toBeTruthy();
+    expect(func(NaN, NaN, NaN)).not.toBeTruthy();
+  });
 });
+
