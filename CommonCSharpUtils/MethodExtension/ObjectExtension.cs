bfa3882785846d446d705576883d559f74029249
OZcomingFRoo <OZcomingFRoo@gmail.com>
2021-04-30 16:38:54 +0300

    added utils C#

---
+++ CommonCSharpUtils/MethodExtension/ObjectExtension.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CommonCSharpUtils.MethodExtension
+{
+    public static class ObjectExtension
+    {
+        public static string ToJSON(this object obj)
+        {
+            var serializer = new DataContractJsonSerializer(obj.GetType());
+            using (var stream = new MemoryStream())
+            {
+                serializer.WriteObject(stream, obj);
+                return Encoding.UTF8.GetString(stream.ToArray());
+            }
+        }
+
+        public static string ToXML(this object obj)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(obj.GetType());
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, obj);
+                return textWriter.ToString();
+            }
+        }
+    }
+}
