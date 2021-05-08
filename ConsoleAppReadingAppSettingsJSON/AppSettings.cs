14454f3a3e5bbe6afedf139f9ea6e279d270529b
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-05-09 01:58:27 +0300

    Added a non-ASP.NET project to show how to read from appsettings.json file from a regular .NET Core project

---
+++ ConsoleAppReadingAppSettingsJSON/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleAppReadingAppSettingsJSON
+{
+    public class AppSettings
+    {
+        public bool InMemory { get; set; }
+        public bool IsProduction { get; set; }
+        public string Title { get; set; }
+        public int SumNumber { get; set; }
+        Arthor Arthor { get; set; }
+    }
+    public class Arthor
+    {
+        public string Name { get; set; }
+        public int SumNumber { get; set; }
+    }
+}
+
---
+++ ConsoleAppReadingAppSettingsJSON/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using ConsoleAppReadingAppSettingsJSON;
+using Microsoft.Extensions.Configuration;
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static AppSettings appSettings = new AppSettings();
+        static void Main(string[] args)
+        {
+            // ------------- How to read from the config file ------------- //
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("MyAppSettings.json", optional: true, reloadOnChange: true);
+            var configuration = builder.Build();
+            ConfigurationBinder.Bind(configuration.GetSection("AppSettings"), appSettings);
+
+            // ------------- The rest of your program here ------------- //
+            // ToDo: how to read from Sub-classes of configuration file
+            Console.WriteLine("InMemory = " + appSettings.InMemory);
+            Console.WriteLine("IsProduction = " + appSettings.IsProduction);
+            Console.WriteLine("Title = " + appSettings.Title);
+        }
+    }
+}
