6b9e8ae58cdd10fc00fa7ec2da809f6bc4dcfa1a
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-05-16 15:16:38 +0300

    Added View Start to Controller View's folder

---
+++ MVC/POCOs/SomeObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.POCOs
+{
+    public class SomeObject
+    {
+        public string Str { get; set; }
+        public int Integer { get; set; }
+        public char Character { get; set; }
+        public bool Boolean { get; set; }
+        public bool [] CheckList { get; set; }
+        public SomeSubObject SubObjectProperty { get; set; }
+    }
+}
