e183f39b6354af7b8be90305183debdd5b75e6e3
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-05-16 19:00:56 +0300

    Updated project MVC with more Explanation on how the special files are used and when they're rendered

---
+++ MVC/ObscureNameSpaceObjects.cs
@@ -0,0 +1,26 @@
+﻿// ================================================================================ //
+// This is used to demonstrate how the ViewImports work when using namespaces to give 
+// In
+// ================================================================================ //
+
+
+namespace SomeRootViewImportObject
+{
+    /// <summary>
+    /// This object can be called directly without writing the namespace.
+    /// This is available for all Views
+    /// </summary>
+    public class ObscureRootObj
+    {
+    }
+}
+namespace SomeRootHomeViewImportObject
+{
+    /// <summary>
+    /// This object can be called directly without writing the namespace.
+    /// This is only present for the Home Ctrl-Views
+    /// </summary>
+    public class ObscureHomeObj
+    {
+    }
+}
