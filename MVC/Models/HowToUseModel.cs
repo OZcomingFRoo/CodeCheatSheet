ef681f6bfe21fd50ba30c222fd28a9d282651fe2
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-05-18 23:39:35 +0300

    done learning htmlHelpers for the time being

--- MVC/Models/HowToUseModel.cs
+++ MVC/Models/HowToUseModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,11 @@ namespace MVC.Models
     /// </summary>
     public class HowToUseModel
     {
+        [Display(Name = "מספר שלם")]
         public int Number { get; set; } = 123;
         public decimal DecimalNumber { get; set; } = 1.37m;
         public double AltDecimalNum { get; set; } = 42.11;
+        [Display(Name = "מחרוזת")]
         public string Sentence { get; set; } = "This property was init by the Model automatically";
         public bool Boolean { get; set; } = true;
         public char Character { get; set; } = 'C';
@@ -27,10 +30,10 @@ namespace MVC.Models
         public Gender Gender { get; set; } = Gender.Female;
 
     }
-    public enum Gender 
+    public enum Gender
     {
         NA = 0,
         Male = 1,
         Female = 2,
-    } 
+    }
 }
