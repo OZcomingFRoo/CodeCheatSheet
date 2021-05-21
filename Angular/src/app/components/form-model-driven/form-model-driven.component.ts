e322dc3c6899e4c4bdc422b8a288a55bfba2d727
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-05-22 00:50:38 +0300

    Angular Commit: added formGroup example

--- Angular/src/app/components/form-model-driven/form-model-driven.component.ts
+++ Angular/src/app/components/form-model-driven/form-model-driven.component.ts
@@ -1,5 +1,6 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, ValidatorFn, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { FormGroup, FormControl, ValidatorFn, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-form-model-driven',
@@ -9,6 +10,17 @@ import { FormControl, ValidatorFn, Validators, AbstractControl, ValidationErrors
 export class FormModelDrivenComponent implements OnInit {
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.firstName.valueChanges.subscribe(this.printValueInConsole,); // Getting value via Observable
+    this.lastName.valueChanges.subscribe(this.printValueInConsole); // Getting value via Observable
+
+    // Add control to FormGroup
+    let carDate = new Date(2016, 1, 5);
+    // Adding values 
+    this.myFormGroup.addControl("manufactureDate", new FormControl(formatDate(carDate, 'yyyy-MM-dd', 'en')));
+    this.myFormGroup.addControl("is2ndHanded", new FormControl(false));
+  }
 
   public firstName: FormControl = new FormControl('Inochi',
     [
@@ -26,23 +38,15 @@ export class FormModelDrivenComponent implements OnInit {
       // Inline Custom Validator
       (control: AbstractControl): ValidationErrors | null => control.value && control.value.trim().toLowerCase() === 'bob' ? { NoBob: { value: control.value } } : null
     ]);
-
   public get FullName(): string {
     // Getting value via property value of FormControl object
     return this.firstName.value + ' ' + this.lastName.value;
   }
-
-  ngOnInit(): void {
-    this.firstName.valueChanges.subscribe(this.printValueInConsole,); // Getting value via Observable
-    this.lastName.valueChanges.subscribe(this.printValueInConsole); // Getting value via Observable
-  }
-
   private printValueInConsole = (val: any) => {
     console.log("Getting value from Observable method..");
     console.log("Value changed to ==> ", val);
   }
-
-  // An Example for a custom Validator
+  /** An Example for a custom Validator */
   private NoBobValidator(): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       let valString: string = control.value;
@@ -50,4 +54,15 @@ export class FormModelDrivenComponent implements OnInit {
       return valString.trim().toLowerCase() === 'bob' ? { NoBob: { value: control.value } } : null;
     }
   };
+
+  public myFormGroup: FormGroup = new FormGroup({
+    // Adding formControls directly
+    carName: new FormControl("Subaru", Validators.required),
+    color: new FormControl(3)
+  });
+  public submitGroup(): void {
+    console.info("Looks like you submitted the groups form");
+    console.info("Is it valid ? ", this.myFormGroup.valid);
+    console.info("Value ", this.myFormGroup.value);
+  }
 }
