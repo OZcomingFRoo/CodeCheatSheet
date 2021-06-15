ba5a8316fda1ca88320107c5894388c8c0ed2099
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-06-15 22:09:32 +0300

    Added more OOP concepts examples

---
+++ CShaprtOOPConcepts/ImmutableClassExample.cs
@@ -0,0 +1,34 @@
+﻿namespace CShaprtOOPConcepts
+{
+    /// <summary>
+    /// This class exibits the examples of an Immutable class<br/>
+    /// Sources : <br/>
+    /// - https://www.c-sharpcorner.com/article/mutable-and-immutable-class-in-c-sharp/ <br/>
+    /// - https://www.codeproject.com/Articles/1043301/Immutable-objects-in-Csharp <br/>
+    /// </summary>
+    class ImmutableClassExample
+    {
+        /// <summary>
+        /// Make the variables <b>read-only</b> so we can not modify the variable after assigning the first time. 
+        /// </summary>
+        private readonly string myStr;
+
+        /// <summary>
+        /// Use <b>parameterized</b> c-tor for assigning the values of the class while creating the object of the class.
+        /// </summary>
+        /// <param name="str">Must provide in a Immutable object as the values that were set <b>cannot be changed</b></param>
+        public ImmutableClassExample(string str)
+        {
+            myStr = str;
+        }
+
+        /// <summary>
+        /// Use properties for getting the variables of the class and remove the setters of the property.<br/>
+        /// Inshort, use only getters. 
+        /// </summary>
+        public string GetStr
+        {
+            get { return myStr; }
+        }
+    }
+}
---
+++ CShaprtOOPConcepts/Struct/CanStructInheritInterface.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CShaprtOOPConcepts.Struct
+{
+    interface IAmSomeStructInteface { void StructInterfaceMethod(); }
+    interface IAmAnotherSomeStructInteface { void AnthoerStructInterfaceMethod(); }
+    /// <summary>
+    /// Can a struct type inherit an interface?<br/>
+    /// Yes it can! and obviosuly can inherit multiple
+    /// </summary>
+    struct CanStructInheritInterface : IAmSomeStructInteface, IAmAnotherSomeStructInteface
+    {
+        public void StructInterfaceMethod()
+        {
+            Console.WriteLine("Yup, you absolutely can");
+        }
+        public void AnthoerStructInterfaceMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
