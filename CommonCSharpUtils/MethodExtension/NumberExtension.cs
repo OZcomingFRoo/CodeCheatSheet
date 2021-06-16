d369b3614bc3c41810b2a02585d62e91701667c5
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-06-16 20:08:36 +0300

    Added Util methods for number types (C# only)

---
+++ CommonCSharpUtils/MethodExtension/NumberExtension.cs
@@ -0,0 +1,42 @@
+﻿namespace CommonCSharpUtils.MethodExtension
+{
+    public static class NumberExtension
+    {
+        public static bool IsWhole(this double num)
+        {
+            return num % 1 == 0;
+        }
+        public static bool IsWhole(this decimal num)
+        {
+            return num % 1 == 0;
+        }
+        public static bool IsWhole(this float num)
+        {
+            return num % 1 == 0;
+        }
+        public static bool IsNatural(this float num)
+        {
+            return num > 0 && num % 1 == 0;
+        }
+        public static bool IsNatural(this double num)
+        {
+            return num > 0 && num % 1 == 0;
+        }
+        public static bool IsNatural(this decimal num)
+        {
+            return num > 0 && num % 1 == 0;
+        }
+        public static bool IsNatural(this int num)
+        {
+            return num > 0;
+        }
+        public static bool IsNatural(this short num)
+        {
+            return num > 0;
+        }
+        public static bool IsNatural(this long num)
+        {
+            return num > 0;
+        }
+    }
+}
