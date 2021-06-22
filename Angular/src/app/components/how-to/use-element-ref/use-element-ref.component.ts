7159dc4bbff27498ab462d303e51e08ce3410154
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-06-22 21:22:23 +0300

    Anugular commit : Added info about ElementRef usage

--- Angular/src/app/components/how-to/use-element-ref/use-element-ref.component.ts
+++ Angular/src/app/components/how-to/use-element-ref/use-element-ref.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-use-element-ref',
   templateUrl: './use-element-ref.component.html',
   styleUrls: ['./use-element-ref.component.css']
 })
-export class UseElementRefComponent implements OnInit {
+export class UseElementRefComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('divRef') divRef!: ElementRef<Element>;
   title = "How to use ElementRef";
   constructor() { }
 
   ngOnInit(): void {
+    // This won't work ! but at least no error will be thrown
+    this.divRef.nativeElement.textContent = "Div was set in NgOnInit";
   }
-
+  ngAfterViewInit() {
+    // Only from this life-cycle you'll be able to access it
+    this.divRef.nativeElement.textContent = "Div was set in ngAfterViewInit";
+  }
 }
