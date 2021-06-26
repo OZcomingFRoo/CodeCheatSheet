e2ba2141fcd843572d9d60eabf8a454ed5e309d7
OZcomingFRoo <32063353+OZcomingFRoo@users.noreply.github.com>
2021-06-26 23:59:08 +0300

    added examples for the decorator HostListener

--- Angular/src/app/components/how-to/host-listener/host-listener.component.ts
+++ Angular/src/app/components/how-to/host-listener/host-listener.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-host-listener',
@@ -8,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class HostListenerComponent implements OnInit {
 
   title = "How to use HostListener Decorator";
+  keyPressCounter: number = 0;
+  scrollCounter: number = 0;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    this.keyPressCounter++;
+  }
+  @HostListener('window:wheel', ['$event'])
+  handleMouseWheel(event: WheelEvent) {
+    console.log(event);
+    this.scrollCounter++;
   }
 
 }
